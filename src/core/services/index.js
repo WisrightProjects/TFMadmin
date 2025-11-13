5b27a0b81cdc9e01a3d9ee519014cfe758374310
agustin-wisright <agustin@wisright.com>
2025-11-13 17:02:29 +0530

    error fixed

--- src/core/services/index.js
+++ src/core/services/index.js
@@ -2,3 +2,4 @@ export {default as userService} from "./auth.service";
 export {default as masterService} from "./master.service";
 export {default as commonService} from "./common.service";
 export {default as usersService} from "./user.service";
+export {default as subscriptionService} from "./subscription.service";
